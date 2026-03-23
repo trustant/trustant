ee1ab73494d5c732e36255be2e81b1c7dac8c9d4
Michele Sciabarra <msciabarra@apache.org>
2026-03-23 19:37:26 +0000

    2026.0323.1924

--- publish.sh
+++ publish.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+cd "$(dirname "$0")"
 git tag -d $(git tag)
 git tag $(date +%Y.%m%d.%H%S)
 TAG=$(git tag)
 git commit -m "$TAG" -a
 git push origin main --tags
+
+OPSROOT="$PWD/olaris-trustable/opsroot.json"
+jq --arg img "$IMAGE:$TAG" '.config.images.trustable = $img' "$OPSROOT" > "$OPSROOT.tmp" && mv "$OPSROOT.tmp" "$OPSROOT"
+jq  -r '.ollama|keys[]' <trustable.json >olaris-trustable/model.lst
+cd olaris-trustable
+git commit -m "$TAG" -a
+git push origin main
