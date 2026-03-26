ea5c7be89e89011f12c4591c590250d1a6088b53
Michele Sciabarra <msciabarra@apache.org>
2026-03-26 12:09:57 +0000

    build 26.085.1257

--- build.sh
+++ build.sh
@@ -14,8 +14,8 @@ jq  -r '.ollama|keys[]' <trustable.json >olaris-trustable/model.lst
 git commit -m "build $TAG" -a
 git tag $TAG
 
-env GOOS=linux GOARCH=amd64 go build -o image/trustable-amd64
-env GOOS=linux GOARCH=arm64 go build -o image/trustable-arm64
+env GOOS=linux GOARCH=amd64 go build -o image/bin/trustable-amd64
+env GOOS=linux GOARCH=arm64 go build -o image/bin.trustable-arm64
 cp -v trustable.json image/trustable.json
 cp -v opencode.md image/opencode.md
 
