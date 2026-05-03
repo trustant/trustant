e780401b39a629fb16298b92d872ea3f998024ad
Trustable User <noreply@example.com>
2026-05-03 12:00:54 +0100

    build trustabledev_0.3.3-alpha_26.123.1254

--- build.sh
+++ build.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 cd "$(dirname $0)"
 if ! test -e ~/Library/Application\ Support/Trustable/id_ed25519
-then echo "This script must be run on Mac after installing Trustable
+then echo "This script must be run on Mac after installing Trustable"
      exit 1
 fi
+
 ID=~/Library/Application\ Support/Trustable/id_ed25519
 IP="$(cat ~/Library/Application\ Support/Trustable/current.ip)"
 
-KEY=${1:-trustable}
+KEY=${1:-trustabledev}
 VERSION=0.3.3-alpha
 IMAGE=ghcr.io/trustable-ai/trustable-app
 TAG="${KEY}_${VERSION}_$(date +%y.%j.%H%S)"
 
 git tag -d $(git tag)
 echo -e "Version: v${VERSION}\nBuild: $TAG\nExpiry: 2026/06/30\n" >version.txt
+git tag $TAG
 
 OPSROOT="./olaris-bestia/opsroot.json"
 jq --arg img "$IMAGE:$TAG" '.config.images.'$KEY' = $img' "$OPSROOT" > "$OPSROOT.tmp" && mv "$OPSROOT.tmp" "$OPSROOT"
+# reread tag
+TAG=$(jq .config.images.$KEY <$OPSROOT -r | awk -F: '{print $2}')
 
 git commit -m "build $TAG" -a
-git tag $TAG
 
 mkdir -p image/bin
 env GOOS=linux GOARCH=amd64 go build -o image/bin/trustable-amd64
@@ -28,22 +31,16 @@ cp -v trustable.json image/trustable.json
 
 image/image.sh "$TAG"
 
+docker save $IMAGE:$TAG | ssh -i "$ID" trustable@"$IP" sudo k3s ctr images import -
+ssh -i "$ID" trustable@"$IP" sudo k3s ctr images list | grep $TAG
 
-ssh -i "$ID" trustable@"$IP" sudo k3s ctr images
+exit 1
 
-#~/.ops/*-*/bin/kind load docker-image $IMAGE:$TAG -n nuvolaris
+#ops trustable trustable redeploy KEY=$KEY
 
-
-ops trustable trustable redeploy KEY=$KEY
-
-cd olaris-trustable
-git commit -m "$TAG" -a
-git tag $TAG
-sleep 3
-ops trustable signin
-echo "Delete and recreate the tests to avoid permission issues"
-echo "do git push origin main --tags to trigger the build"
-echo "check here https://github.com/trustable-ai/trustable-app/actions when the build completes"
-echo "do cd olaris-trustable ; git push origin main --tags to publish the plugin when ready"
+#cd olaris-trustable
+#git commit -m "$TAG" -a
+#git tag $TAG
+#sleep 3
 
 
--- setup.md
+++ setup.md
@@ -12,8 +12,7 @@ and the folder pointed by $WORKBENCH_DIR exists
 - OPS_REPO=https://github.com/nuvolaris/bestia
 - OPS_BRANCH=bestia
 
-If not, recommend to set the variables
-then install ops with:
+If not, recommend to set the variables, then install ops with:
 
 curl -sL n7s.co/get-ops (mac/win)
 powershell -c "irm n7s.co/get-ops | iex" (win)
@@ -40,14 +39,20 @@ verify  curl -sL <apihost>/api/info | jq .description returns OpenWhisk
 
 6. On mac, if there is the file ~/Library/Application Support/Trustable/id_ed25519,
 
-ID=~/Library/Application\ Support/Trustable/id_ed25519
 IP="$(cat ~/Library/Application\ Support/Trustable/current.ip)"
-ssh -i "$ID" trustable@$IP "$@" sudo cat /etc/rancher/k3s/k3s.yaml | sed -e "/server:/ s/127.0.0.1/$IP/" >~/.ops/tmp/kubeconfig
+./ssh.sh sudo cat /etc/rancher/k3s/k3s.yaml | sed -e "/server:/ s/127.0.0.1/$IP/" >~/.ops/tmp/kubeconfig
 
 7. check you have administrative power
 ensuring `ops admin listuser` does not return error
 
-8. if it is nt there, install opencode in ~/.opencode/bin with `curl -fsSL https://opencode.ai/install | bash`
+8. if it is not there, install opencode in ~/.opencode/bin with `curl -fsSL https://opencode.ai/install | bash`
 
 9. Check you have a local ollama running with http://localhost:11434 returning Ollama is Running
 
+10. preload the ollama image if it is not yet in the vm:
+
+if ! ./ssh.sh sudo k3s images list | grep ollama
+then
+    OLLAMA=$(jq .config.images.ollama -r  olaris-bestia/opsroot.json)
+    docker pull $OLLAMA
+fi
