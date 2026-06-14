2f1f080e7844025d7693eab47abd004492ff2648
Trustable User <noreply@example.com>
2026-06-14 14:31:44 +0200

    build trustable_v0.3.8_26.165.1431

--- build.sh
+++ build.sh
@@ -44,7 +44,6 @@ docker save $IMAGE:$TAG | ssh -i "$ID" trustable@"$IP" sudo k3s ctr images impor
 
 echo "Listing Images"
 
-ssh -i "$ID" trustable@"$IP"
 
 ssh -i "$ID" trustable@"$IP" sudo k3s ctr images list | grep trustable
 
