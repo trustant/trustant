28d9e78bf994267fc398805d2c8c679f5dc4fb46
Trustable User <noreply@example.com>
2026-06-20 15:50:47 +0200

    fixing extra lines in kubeconfig

--- image/start.sh
+++ image/start.sh
@@ -6,13 +6,10 @@ ssh-keygen -A
 export HOME=/home/trustable
 export PATH="$HOME/.local/bin:$PATH"
 source $HOME/.bashrc
-ops -info
 
 if test -n "$B64KUBECONFIG"
-then ops -base64 -d "$B64KUBECONFIG"  >$HOME/.ops/tmp/kubeconfig
+then echo "$B64KUBECONFIG" | base64 -d -w0 >$HOME/.ops/tmp/kubeconfig
 fi
-echo "Checking kubeconfig"
-ls -l ~/.ops/tmp/kubeconfig
 
 if [ -n "$USERID" ] && [ "$USERID" != "769" ]
 then /usr/sbin/usermod -u $USERID trustable
